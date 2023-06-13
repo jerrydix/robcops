bbad6801abcc4c37d166fa0044fcd6065915d438
MichailZh <37882050+MichailZh@users.noreply.github.com>
2023-06-13 23:36:15 +0200

    New Classes: Guild, Safe and BreakInEvent

--- Server/ServerSide/admin.py
+++ Server/ServerSide/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Player
 from .models import FriendRequest
 from .models import Guild
+from .models import Safe
+from .models import BreakInEvent
 
 admin.site.register(Player)
 admin.site.register(FriendRequest)
 admin.site.register(Guild)
+admin.site.register(Safe)
+admin.site.register(BreakInEvent)
--- Server/ServerSide/models.py
+++ Server/ServerSide/models.py
@@ -5,9 +5,24 @@ from django.db import models
 # Current models: Player, Safe, Break_In_Event, Rob_Union, Police_Station
 
 
+class Safe(models.Model):
+    level = models.IntegerField(default=1)
+    hp = models.IntegerField(default=100)
+    locationX = models.FloatField(default=0.0)
+    locationY = models.FloatField(default=0.0)
+
+
+class BreakInEvent(models.Model):
+    safe = models.OneToOneField(Safe, on_delete=models.CASCADE, primary_key=True)
+
+
 class Guild(models.Model):
+    name = models.CharField(default="NoNameGuild", max_length=30)
     guildMoney = models.IntegerField(default=0)
     machines = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.guild.name
 
 
 class Player(models.Model):
@@ -20,6 +35,7 @@ class Player(models.Model):
     locationY = models.FloatField(default=0.0)
     role = models.BooleanField(default=0)
     guild = models.ForeignKey(Guild, null=True, blank=True, related_name='guild', on_delete=models.SET_NULL)
+    event = models.ForeignKey(BreakInEvent, null=True, blank=True, related_name='event', on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.user.username
