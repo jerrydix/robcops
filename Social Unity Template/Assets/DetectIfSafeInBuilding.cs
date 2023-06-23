028ed3a42a2e2957936bb8a7effdfe7c22ce0c87
derzport <thederzport@gmail.com>
2023-06-23 20:13:19 +0200

    Update on the DetectionSystem Not done

--- Social Unity Template/Assets/DetectIfSafeInBuilding.cs
+++ Social Unity Template/Assets/DetectIfSafeInBuilding.cs
@@ -1,22 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mapbox.Examples;
 using Mapbox.Unity.MeshGeneration.Data;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class DetectIfSafeInBuilding : MonoBehaviour
 {
+   private SpawnOnMap _spawnOnMap;
+   private List<GameObject> _yeetedSafes;
+
+   private void Awake()
+   {
+      _yeetedSafes = new List<GameObject>();
+      _spawnOnMap = GameObject.FindWithTag("Spawner").GetComponent<SpawnOnMap>();
+   }
+
+   void Start()
+   {
+      
+   }
+
    
 
+
+ 
+
+
+   /*
    private void OnTriggerEnter(Collider other)
    {
       if (other.gameObject.CompareTag("Building"))
       {
          Debug.Log("Connection");
+         _yeetedSafes.Add(gameObject);
+         _spawnOnMap._spawnedObjects.Remove(gameObject);
          gameObject.Destroy();
       }
    }
+   */
 
    private void OnTriggerExit(Collider other)
    {
--- Social Unity Template/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Social Unity Template/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Mapbox.Examples
 {
@@ -18,27 +19,68 @@ namespace Mapbox.Examples
 		[Geocode]
 		public string[] _locationStrings;
 		Vector2d[] _locations;
+		private Vector2d[] _cubeLocations;
 
 		[SerializeField]
 		float _spawnScale = 100f;
+
+		private bool activateUpdate = false;
 		
 
-		[SerializeField]
-		GameObject _markerPrefab;
+		
+		public GameObject _markerPrefab;
 
 		private SafeSpinScript SafeSpinScript;
 
 		public List<GameObject> _spawnedObjects;
+		private List<GameObject> cubes = new List<GameObject>();
 
 		
-	
 
 		void Start()
 		{
-			_spawnedObjects = new List<GameObject>();
+			CheckCollision();
+			StartCoroutine(waitTest());
+		}
+
+		private void FixedUpdate()
+		{
+			UpdateLocation();
+		}
+
+		private void Update()
+		{
+			if (activateUpdate)
+			{
+				safeLocation();
+			}
+		}
+
+		private void safeLocation()
+		{
+			int count = _spawnedObjects.Count;
+			for (int i = 0; i < count; i++)
+			{
+				var spawnedObject = _spawnedObjects[i];
+				
+				var location = _locations[i];
+				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+				
+			}
+		}
+
+		private IEnumerator waitTest()
+		{
+			yield return new WaitForSeconds(5);
 			_locations = new Vector2d[_locationStrings.Length];
+			_spawnedObjects = new List<GameObject>();
 			for (int i = 0; i < _locationStrings.Length; i++)
 			{
+				if (_locationStrings[i] == null)
+				{
+					continue;
+				}
 				var locationString = _locationStrings[i];
 				_locations[i] = Conversions.StringToLatLon(locationString);
 				var instance = Instantiate(_markerPrefab);
@@ -46,20 +88,58 @@ namespace Mapbox.Examples
 				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
 				_spawnedObjects.Add(instance);
 			}
+
+			activateUpdate = true;
+
 		}
 
-		private void Update()
+
+
+		private void UpdateLocation()
 		{
-			int count = _spawnedObjects.Count;
+			RaycastHit hit;
+			int count = cubes.Count;
 			for (int i = 0; i < count; i++)
 			{
-				var spawnedObject = _spawnedObjects[i];
-				
-					var location = _locations[i];
-					spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-					spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				
+				var test = cubes[i];
+				var location = _cubeLocations[i];
+				test.transform.localPosition = _map.GeoToWorldPosition(location, true);
+				test.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+				var temp = test.transform.position;
+				temp.y += 5;
+				if (Physics.Raycast(temp,Vector3.down,out hit,Mathf.Infinity))
+				{
+					if (hit.collider.gameObject.CompareTag("Building"))
+					{
+						Debug.Log(hit.collider.gameObject);
+						_locationStrings[i] = null;
+						Debug.Log("PLS");
+					}
+					
+				}
 			}
+		}
+		private void CheckCollision()
+		{
+			_cubeLocations = new Vector2d[_locationStrings.Length];
+			for (int i = 0; i < _locationStrings.Length; i++)
+			{
+				
+				var location = _locationStrings[i];
+				_cubeLocations[i] = Conversions.StringToLatLon(location);
+				var pos = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				pos.GetComponent<Collider>().enabled = false;
+				pos.GetComponent<MeshRenderer>().enabled = false;
+				pos.transform.position = _map.GeoToWorldPosition(_cubeLocations[i],true);
+				pos.transform.position = new Vector3(pos.transform.position.x, pos.transform.position.y + 2,
+					pos.transform.position.z);
+				pos.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+				cubes.Add(pos);
+				
+
+			}
+			
+			
 		}
 		
 		
