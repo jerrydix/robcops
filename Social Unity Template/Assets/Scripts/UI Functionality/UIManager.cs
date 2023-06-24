738a671ff64ed06162d9d56cb8530c745d0c9aa6
J3rry <jeremydix17@gmail.com>
2023-06-25 01:42:00 +0200

    Added UI page structure
    - guild
    - shop
    - settings

--- Social Unity Template/Assets/Scripts/UI Functionality/UIManager.cs
+++ Social Unity Template/Assets/Scripts/UI Functionality/UIManager.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private SafeDialogue dialogue;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject mapScreenUI;
+    [SerializeField] private GameObject guildUI;
+    [SerializeField] private GameObject shopUI;
+    [SerializeField] private GameObject settingsUI;
 
     public void ActivateDialogue(string safeDescriptionText, string addressText, string lobbyText)
     {
         dialogue.gameObject.SetActive(true);
         dialogue.InitializeSafe(safeDescriptionText, addressText, lobbyText);
     }
+
+    public void GuildButton()
+    {
+        mapScreenUI.SetActive(false);
+        guildUI.SetActive(true);
+    }
+
+    public void ShopButton()
+    {
+        mapScreenUI.SetActive(false);
+        shopUI.SetActive(true);
+    }
+
+    public void SettingsButton()
+    {
+        mapScreenUI.SetActive(false);
+        settingsUI.SetActive(true);
+    }
+
+    public void BackToMapButton()
+    {
+        settingsUI.SetActive(false);
+        shopUI.SetActive(false);
+        guildUI.SetActive(false);
+        mapScreenUI.SetActive(true);
+    }
+
 }
