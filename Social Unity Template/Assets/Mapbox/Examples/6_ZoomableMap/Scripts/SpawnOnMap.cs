48726d5baf595c6225316b4e4250c4f652ad4141
J3rry <jeremydix17@gmail.com>
2023-07-08 23:06:39 +0200

    Update SpawnOnMap.cs

--- Social Unity Template/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Social Unity Template/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -106,7 +106,7 @@ namespace Mapbox.Examples
 
         private IEnumerator UpdateCubeLocation()
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0); //TODO CHECK IF THIS BREAKS ANYTHING
             var count = cubes.Count;
             for (var i = 0; i < count; i++)
             {
