f8152752b63b0d3e31d9c00f0ce77e4f8c9f46ef
RiusLu <95547149+RiusLu@users.noreply.github.com>
2023-07-13 23:17:29 +0200

    Well souund is fixed, but the sliders are non functional. in theory every buttuon that should make a sound makes a sound now c:

--- Social Unity Template/Assets/Scripts/Audio/Sound.cs
+++ Social Unity Template/Assets/Scripts/Audio/Sound.cs
@@ -8,15 +8,15 @@ public class Sound
 {
     public string name;
     public AudioClip clip;
-    [Range(0f, 100f)] public float volume;
-    [Range(0f, 100f)] public float pitch;
+    [Range(0f, 1f)] public float volume;
+    [Range(.1f, 3f)] public float pitch;
 
     public bool loop;
     
     [HideInInspector] public AudioSource source;
 
 
-    public void setVolume(float n)
+    public void SetVolume(float n)
     {
         volume = n;
     }
