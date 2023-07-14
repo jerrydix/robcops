8894d40c1e59913d84c93748b59d0c04d03afb90
RiusLu <95547149+RiusLu@users.noreply.github.com>
2023-07-14 22:08:29 +0200

    Revert "Audio Works yay, Only a sound track is missing"
    This reverts commit 0748a0b86b56ab14163c25deb743925e740382d2.

--- Social Unity Template/Assets/Scripts/Audio/AudioManager.cs
+++ Social Unity Template/Assets/Scripts/Audio/AudioManager.cs
@@ -6,197 +6,115 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public Sound[] sounds;
     public static AudioManager instance;
     
-    public Sound[] musicSounds,sfxSounds;
-    public AudioSource musicSource,sfxSource;
+    public bool soundMuted;
+    public bool musicMuted;
+    
 
+    [SerializeField] public GameObject soundVolumeSlider;
+    [SerializeField] public GameObject musicVolumeSlider;
+    [SerializeField] public GameObject soundVolumeButton;
+    [SerializeField] public GameObject musicVolumeButton;
+
+    [SerializeField] public GameObject SettingsUI;
+    
 
     public void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
-            
-        }
-    }
-    
-    private void Start()
-    {
-        PlayMusic("Theme");
-    }
-    
-    
-
-    public void PlayMusic(string name)
-    {
-        Sound s = Array.Find(musicSounds, x => x.name == name);
-        if (s == null)
-        {
             return;
         }
 
-        musicSource.clip = s.Clip;
-        musicSource.Play();
+        DontDestroyOnLoad(gameObject);
+
+        musicMuted = false;
+        soundMuted = false;
         
-    }
-    
-    public void PlaySFX(string name)
-    {
-        Sound s = Array.Find(sfxSounds, x => x.name == name);
-        if (s == null)
-        {
-            return;
-        }
-
-        sfxSource.PlayOneShot(s.Clip);
-        
-    }
-
-    public void ToggleMusic()
-    {
-        musicSource.mute = !musicSource.mute;
-    }
-
-    public void ToggleSfx()
-    {
-        sfxSource.mute = !sfxSource.mute;
-    }
-
-    public void MusicVolume(float volume)
-    {
-        musicSource.volume = volume;
-    }
-
-    public void SfxVolume(float volume)
-    {
-        sfxSource.volume = volume;
-    }
-
-
-
-
-
-    public void ButtonPressed()
-    {
-        PlaySFX("ButtonPressed");
-    }
-
-}
-/*public static AudioManager instance;
-
-public bool soundMuted;
-public bool musicMuted;
-
-
-[SerializeField] public GameObject soundVolumeSlider;
-[SerializeField] public GameObject musicVolumeSlider;
-[SerializeField] public GameObject soundVolumeButton;
-[SerializeField] public GameObject musicVolumeButton;
-
-[SerializeField] public GameObject SettingsUI;
-
-
-public void Awake()
-{
-    if (instance == null)
-    {
-        instance = this;
-    }
-    else
-    {
-        Destroy(gameObject);
-        return;
-    }
-
-    DontDestroyOnLoad(gameObject);
-
-    musicMuted = false;
-    soundMuted = false;
-    
-    foreach (var s in sounds)
-    {
-        s.source = gameObject.AddComponent<AudioSource>();
-        s.source.clip = s.clip;
-
-        s.source.volume = s.volume;
-        s.source.pitch = s.pitch;
-        s.source.loop = s.loop;
-    }
-    soundVolumeSlider.GetComponent<Slider>().value = Array.Find(sounds, sound => sound.name == "ButtonPressed").volume;
-    musicVolumeSlider.GetComponent<Slider>().value = Array.Find(sounds, sound => sound.name == "Theme").volume;
-}
-
-private void Start()
-{
-    Play("Theme");
-}
-
-private void Update()
-{
-    Array.Find(sounds, sound => sound.name == "ButtonPressed").SetVolume(soundVolumeSlider.GetComponent<Slider>().value) ;
-        Array.Find(sounds, sound => sound.name == "Theme").volume = musicVolumeSlider.GetComponent<Slider>().value;
         foreach (var s in sounds)
         {
-            s.source.volume = s.volume;
-            //s.source.pitch = s.pitch;
-            //s.source.loop = s.loop;
-        }
-}
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
 
-public void Play(string name)
-{
-    Sound s = Array.Find(sounds, sound => sound.name == name);
-    if (s == null)
-    {
-        return;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        soundVolumeSlider.GetComponent<Slider>().value = Array.Find(sounds, sound => sound.name == "ButtonPressed").volume;
+        musicVolumeSlider.GetComponent<Slider>().value = Array.Find(sounds, sound => sound.name == "Theme").volume;
     }
 
-    s.source.Play();
+    private void Start()
+    {
+        Play("Theme");
+    }
+
+    private void Update()
+    {
+        Array.Find(sounds, sound => sound.name == "ButtonPressed").SetVolume(soundVolumeSlider.GetComponent<Slider>().value) ;
+            Array.Find(sounds, sound => sound.name == "Theme").volume = musicVolumeSlider.GetComponent<Slider>().value;
+            foreach (var s in sounds)
+            {
+                s.source.volume = s.volume;
+                //s.source.pitch = s.pitch;
+                //s.source.loop = s.loop;
+            }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+
+        s.source.Play();
+        
+    }
+
+    public void ButtonPressedSound()
+    {
+        if (soundMuted == false)
+        {
+            Play("ButtonPressed");
+        }
+    }
+
+    public void MusicSwitch()
+    {
+        musicMuted = !musicMuted;
+        if (musicMuted == true)
+        {
+            musicVolumeSlider.SetActive(false);
+            musicVolumeButton.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            musicVolumeSlider.SetActive(true);
+            musicVolumeButton.GetComponent<Image>().color = Color.white;
+        }
+    }
+    public void SoundSwitch()
+    {
+        soundMuted = !soundMuted;
+        if (soundMuted == true)
+        {
+            soundVolumeSlider.SetActive(false);
+            soundVolumeButton.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            soundVolumeSlider.SetActive(true);
+            soundVolumeButton.GetComponent<Image>().color = Color.white;
+        }
+    }
     
 }
-
-public void ButtonPressedSound()
-{
-    if (soundMuted == false)
-    {
-        Play("ButtonPressed");
-    }
-}
-
-public void MusicSwitch()
-{
-    musicMuted = !musicMuted;
-    if (musicMuted == true)
-    {
-        musicVolumeSlider.SetActive(false);
-        musicVolumeButton.GetComponent<Image>().color = Color.red;
-    }
-    else
-    {
-        musicVolumeSlider.SetActive(true);
-        musicVolumeButton.GetComponent<Image>().color = Color.white;
-    }
-}
-public void SoundSwitch()
-{
-    soundMuted = !soundMuted;
-    if (soundMuted == true)
-    {
-        soundVolumeSlider.SetActive(false);
-        soundVolumeButton.GetComponent<Image>().color = Color.red;
-    }
-    else
-    {
-        soundVolumeSlider.SetActive(true);
-        soundVolumeButton.GetComponent<Image>().color = Color.white;
-    }
-}
-
-}*/
--- Social Unity Template/Assets/Scripts/Audio/Sound.cs
+++ Social Unity Template/Assets/Scripts/Audio/Sound.cs
@@ -7,5 +7,17 @@ using UnityEngine;
 public class Sound
 {
     public string name;
-    public AudioClip Clip;
+    public AudioClip clip;
+    [Range(0f, 1f)] public float volume;
+    [Range(.1f, 3f)] public float pitch;
+
+    public bool loop;
+    
+    [HideInInspector] public AudioSource source;
+
+
+    public void SetVolume(float n)
+    {
+        volume = n;
+    }
 }
